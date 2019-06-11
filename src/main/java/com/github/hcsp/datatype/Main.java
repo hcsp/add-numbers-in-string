80a77d62e85495ff51209fa33dfa0fdccd2d2770
zhangyongwang <20225614@qq.com>
2019-06-11 19:17:54 +0800

    将字符串中的数字相加

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -13,13 +13,13 @@ public class Main {
     public static String add(String a, String b) {
         if (a == null) {
             a = "0";
-        } ;
+        }
+        ;
         if (b == null) {
             b = "0";
-        } ;
+        }
+        ;
         int res = Integer.valueOf(a) + Integer.valueOf(b);
-
         return String.valueOf(res);
-
     }
 }
