849f74c4d3064a31612ef42a490fdcafa3bab141
hanchenhao <hanchenhao@vip.qq.com>
2020-05-16 19:00:24 +0800

    add

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,5 +9,11 @@ public class Main {
     // 给出两个数字字符串a和b，返回其中的数字相加后的字符串结果。
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        return String.valueOf(string2Float(a) + string2Float(b));
+    }
+
+    private static Float string2Float(String str) {
+        return str == null ? 0 : Float.valueOf(str);
+    }
 }
