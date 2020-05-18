41a8c9f6f298c917edb438aed6a790875f165806
fuduo <329794799@qq.com>
2020-05-19 03:05:19 +0800

    将字符串中的数字相加

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -10,34 +10,12 @@ public class Main {
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
     public static String add(String a, String b) {
-        int i = 0;
-        int j = 0;
-        if (a == null) {
-            i = 0;
-        } else {
-            int helpA = 1;
-            for (int indexOfA = a.length() - 1; indexOfA >= 0; indexOfA --) {
-                if (a.charAt(indexOfA) == '-') {
-                    i *= -1;
-                    continue;
-                }
-                i += (a.charAt(indexOfA) - 48) * helpA;
-                helpA *= 10;
-            }
+        return String.valueOf(valueOfString(a) + valueOfString(b));
+    }
+    public static int valueOfString(String str) {
+        if (str == null) {
+            return 0;
         }
-        if (b == null) {
-            j = 0;
-        } else {
-            int helpB = 1;
-            for (int indexOfB = b.length() - 1; indexOfB >= 0; indexOfB --) {
-                if (b.charAt(indexOfB) == '-') {
-                    j *= -1;
-                    continue;
-                }
-                j += (b.charAt(indexOfB) - 48) * helpB;
-                helpB *= 10;
-            }
-        }
-        return String.valueOf(i + j);
+        return Integer.parseInt(str);
     }
 }
