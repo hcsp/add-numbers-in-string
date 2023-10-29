69b218fed991f009754a160a0f51a26ca2d5dc83
lyraaaaaaa <Lyraaaaaaaaaaa@gmail.com>
2023-10-29 22:43:51 +0800

    3

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,7 +1,7 @@
 package com.github.hcsp.datatype;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main {                                  //main类
+    public static void main(String[] args) {           //程序入口
         System.out.println(add("123", "456"));
         System.out.println(add("123", null));
         System.out.println(add(null, null));
@@ -9,5 +9,17 @@ public class Main {
     // 给出两个数字字符串a和b，返回其中的数字相加后的字符串结果。
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+       if(a == null){
+           a = "0";
+       }
+       if (b == null){
+           b = "0";
+       }
+       int m = Integer.parseInt(a);
+       int n = Integer.parseInt(b);
+       int i = m + n;
+       return String.valueOf(i);
+    }
 }
+
